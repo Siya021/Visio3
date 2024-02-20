8673a7d091c78b9f78e8b5275be48ac8f27106f5
Siyamthanda Mazokwana <siyamthandamazokwana@gmail.com>
2024-02-20 10:27:14 +0200

    final

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
-    $(".nxtBtn").on("click", function () {
-      let currentImg = $(".img-container .active");
-      let nextImg = currentImg.next();
+    // Define the interval for the slideshow (in milliseconds)
+    const interval = 3000; // Change this value as needed
   
-
-      if (!nextImg.length) {
-        nextImg = $(".img-container img").first();
-      }
+    // Function to start the slideshow
+    function startSlideshow() {
+      setInterval(function () {
+        let currentImg = $(".img-container .active");
+        let nextImg = currentImg.next();
   
-      currentImg.removeClass('active').css('z-index', -1);
-      nextImg.addClass('active').css('z-index', 1);
-    });
+        // If there is no next image, loop back to the first image
+        if (!nextImg.length) {
+          nextImg = $(".img-container img").first();
+        }
+  
+        currentImg.removeClass('active').css('z-index', -1);
+        nextImg.addClass('active').css('z-index', 1);
+      }, interval);
+    }
+  
+    // Start the slideshow when the document is ready
+    startSlideshow();
   });
   
