14761590b4cbf37ace39cf16ff22a9edccd59bd9
Siyamthanda Mazokwana <siyamthandamazokwana@gmail.com>
2024-02-23 09:56:51 +0200

    fix

--- index.js
+++ index.js
@@ -1,60 +1,67 @@
-// $(document).ready(function () {
-//     // Define the interval for the slideshow (in milliseconds)
-//     const interval = 3000; // Change this value as needed
-  
-//     // Function to start the slideshow
-//     function startSlideshow() {
-//       setInterval(function () {
-//         let currentImg = $(".img-container .active");
-//         let nextImg = currentImg.next();
-  
-//         // If there is no next image, loop back to the first image
-//         if (!nextImg.length) {
-//           nextImg = $(".img-container img").first();
-//         }
-  
-//         currentImg.removeClass('active').css('z-index', -1);
-//         nextImg.addClass('active').css('z-index', 1);
-//       }, interval);
-//     }
-  
-//     // Start the slideshow when the document is ready
-//     startSlideshow();
-//   });
 $(document).ready(function () {
-    $(".arrow").on("click", function () {
-      var currentImg = $(".slidein img.active");
-      var nextImg = currentImg.next();
-  
-      if (nextImg.length === 0) {
-        nextImg = $(".slidein img:first");
-      }
-  
-      currentImg.removeClass("active").css("z-index", -10);
-      nextImg.addClass("active").css("z-index", 10);
-    });
-  
-    $(".prev").on("click", function () {
-      var currentImg = $(".slidein img.active");
-      var prevImg = currentImg.prev();
-  
-      if (prevImg.length === 0) {
-        prevImg = $(".slidein img:last");
-      }
-  
-      currentImg.removeClass("active").css("z-index", -10);
-      prevImg.addClass("active").css("z-index", 10);
-    });
-  
+    const interval = 3000; // Interval for automatic slideshow (in milliseconds)
+
+    // Function to start the slideshow
+    function startSlideshow() {
+        setInterval(function () {
+            var currentImg = $(".slidein img.active");
+            var nextImg = currentImg.next();
+
+            if (nextImg.length === 0) {
+                nextImg = $(".slidein img:first");
+            }
+
+            currentImg.removeClass("active").css("z-index", -10);
+            nextImg.addClass("active").css("z-index", 10);
+        }, interval);
+    }
+
+    // Start the slideshow when the document is ready
+    startSlideshow();
+
+    // Manual control for next slide
     $(".next").on("click", function () {
-      var currentSVG = $(".radio-btn .activeSVG");
-      var nextSVG = currentSVG.next();
-  
-      if (nextSVG.length === 0) {
-        nextSVG = $(".radio-btn img:first");
-      }
-  
-      currentSVG.removeClass("activeSVG");
-      nextSVG.addClass("activeSVG");
+        var currentSVG = $(".radio-btn .activeSVG");
+        var nextSVG = currentSVG.next();
+
+        if (nextSVG.length === 0) {
+            nextSVG = $(".radio-btn img:first");
+        }
+
+        currentSVG.removeClass("activeSVG");
+        nextSVG.addClass("activeSVG");
+
+        var currentImg = $(".slidein img.active");
+        var nextImg = currentImg.next();
+
+        if (nextImg.length === 0) {
+            nextImg = $(".slidein img:first");
+        }
+
+        currentImg.removeClass("active").css("z-index", -10);
+        nextImg.addClass("active").css("z-index", 10);
     });
-  });
+
+    // Manual control for previous slide
+    $(".prev").on("click", function () {
+        var currentSVG = $(".radio-btn .activeSVG");
+        var prevSVG = currentSVG.prev();
+
+        if (prevSVG.length === 0) {
+            prevSVG = $(".radio-btn img:last");
+        }
+
+        currentSVG.removeClass("activeSVG");
+        prevSVG.addClass("activeSVG");
+
+        var currentImg = $(".slidein img.active");
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length === 0) {
+            prevImg = $(".slidein img:last");
+        }
+
+        currentImg.removeClass("active").css("z-index", -10);
+        prevImg.addClass("active").css("z-index", 10);
+    });
+});
